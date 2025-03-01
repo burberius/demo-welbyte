d5b39d4ed4a23c54a22fa901fd4c51af4f5ea09d
Jens Oberender <burberius@users.noreply.github.com>
2025-03-01 06:51:27 +0100

    First version with db

---
+++ src/main/java/net/troja/demo_welbyte/DemoWelbyteApplication.java
@@ -0,0 +1,13 @@
+package net.troja.demo_welbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWelbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWelbyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/troja/demo_welbyte/DemoWelbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.troja.demo_welbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWelbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
